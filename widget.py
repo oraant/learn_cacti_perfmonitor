6071025e98b09a77d2df061d110abddb69463f86
oraant <oraant777@gmail.com>
2015-06-07 15:38:31 +0800

    add widget.py,add getfiles function

--- widget.py
+++ widget.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-print 'asd'
-print '你好'
+
+import ConfigParser
+import logging
+import dbm
+import commands
+import base64
+import sys
+
+def getfiles(flag):
+	path_conf_global = sys.path[0] + '/conf/global.conf'
+	path_conf = sys.path[0] + '/conf/' + flag + '.conf'
+	path_log  = sys.path[0] + '/log/' + flag + '.log'
+	path_dbm  = sys.path[0] + '/dbm/' + flag
+
+	cf_global = ConfigParser.ConfigParser()
+	cf_global.read(path_conf_global)
+	log_level = cf_global.getint('server','log_level')
+
+	cf = ConfigParser.ConfigParser()
+	cf.read(path_conf)
+
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s -- %(message)s')
+	fh = logging.FileHandler(path_log)
+	fh.setFormatter(formatter)
+
+	logger = logging.getLogger(flag)
+	logger.setLevel(log_level)
+	logger.addHandler(fh)
+
+	dbm = dbm.open(path_dbm,'c')
