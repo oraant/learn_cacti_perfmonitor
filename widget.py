b2e1100100b4612a157475dc022213a858ec2975
oraant <oraant777@gmail.com>
2015-06-07 18:37:52 +0800

    add decrypt, encrypt, get slink, verify mac,and verify model enable.five function

--- widget.py
+++ widget.py
@@ -8,6 +8,7 @@ import commands
 import base64
 import sys
 
+#get config,dbm and logger
 def getfiles(flag):
 	path_conf_global = sys.path[0] + '/conf/global.conf'
 	path_conf = sys.path[0] + '/conf/' + flag + '.conf'
@@ -32,3 +33,44 @@ def getfiles(flag):
 	data = dbm.open(path_dbm,'c')
 
 	return cf,data,logger
+
+
+#decrypt or encrypt one string
+def decrypt(string):
+	return base64.decodestring(base64.decodestring(base64.decodestring(base64.decodestring(string))))
+def encrypt(string):
+	return base64.encodestring(base64.encodestring(base64.encodestring(base64.encodestring(string))))
+
+
+#get slink from config file
+conf,data,logger = getfiles('global')
+def getslink():
+	return conf.get('server','slink')
+
+
+#if a model can run
+def verifymac():
+	cf_verify = ConfigParser.ConfigParser()
+	cf_verify.read(sys.path[0] + '/conf/verify.conf')
+	mac = decrypt(cf_verify.get("mac", "mac"))
+
+	command = "/sbin/ifconfig -a|grep HWaddr|head -1|awk '{print $NF}'"
+	(status,output) = commands.getstatusoutput(command)
+
+	if status != 0:
+		logger.error('can\'t get mac from linux,output is : ' + output)
+		return False
+	elif output != mac:
+		logger.error('wrong mac,configure is ' + mac + ',ifconfig is ' + output)
+		return False
+	else:
+		return True
+
+def verifyenable(flag):
+	if conf.getboolean('enable','global') != True:
+		return False
+	if conf.getboolean('enable',flag) != True:
+                return False
+	if verifymac() != True:
+                return False
+	return True
