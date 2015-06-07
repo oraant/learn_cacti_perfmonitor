3e0d89ad29d6da501c5d74e15afd584a349d5f66
oraant <oraant777@gmail.com>
2015-06-07 19:02:14 +0800

    test for name space

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -2,21 +2,12 @@
 #-*- coding:utf-8 -*-
 
 import inc
-import logging
 
+v = 'aaa'
+print v
 
-global_logger = logging.getLogger('global')
-log_level = 40
-global_logger.setLevel(log_level)
+def f(v,b):
+	print v
+	print b
 
-global_fh = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s -- %(message)s')
-global_fh.setFormatter(formatter)
-
-global_logger.addHandler(global_fh)
-
-global_logger.debug("debug message")  
-global_logger.info("info message")  
-global_logger.warn("warn message")  
-global_logger.error("error message")  
-global_logger.critical("critical message")
+f('ooo','bbb')
