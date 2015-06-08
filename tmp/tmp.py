a5304f0d0fdeaeb7dec5e7e8a8a5e673bc121e3a
oraant <oraant777@gmail.com>
2015-06-08 22:31:15 +0800

    test for functions with _

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -3,22 +3,11 @@
 
 import inc
 import time
-import cx_Oracle
 
-sql = 'select * from dual where 2=1'
-link = 'system/oracle@192.168.56.60:1521/db11g'
+def f():
+	print 'a'
+	_ff()
+def _ff():
+	print 'b'
 
-db = cx_Oracle.connect(link)
-cursor = db.cursor()
-cursor.execute(sql)
-r = cursor.fetchall()
-
-print 'r ='
-print r
-print type(r)
-print len(r)
-
-for i in r:
-	print 'i ='
-	print i
-	print type(i)
+f()
