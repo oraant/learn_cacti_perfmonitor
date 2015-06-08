477280c10bdfbf973b5f9cb2dc2aaa8270406a02
oraant <oraant777@gmail.com>
2015-06-09 07:18:06 +0800

    Format the function file,and add some annotation in it.

--- setup/b_base_functions.sql
+++ setup/b_base_functions.sql
@@ -1,90 +1,79 @@
-create or replace function to_file(i_target_name varchar2,
-                                   i_param_table varchar2,
-                                   i_file_dir    varchar2,
-                                   i_file_name   varchar2) return varchar2 is
-  v_file_cur  Utl_File.file_type;
-  v_timestamp number(20);
-
-  v_param_count number;
-
-  v_class_count number;
-
-  v_param_name varchar2(200);
-  v_value_char varchar2(100);
-
-  temp number;
-
-begin
-
-  select (sysdate - TO_DATE('19700101', 'yyyymmdd')) * 86400 -
-         TO_NUMBER(SUBSTR(TZ_OFFSET(sessiontimezone), 1, 3)) * 3600
-    into v_timestamp
-    from dual;
-
-  execute immediate 'select max(class_id) from ' || i_param_table
-    into v_class_count;
-
-  for v_temp1 in 1 .. v_class_count loop
+CREATE OR REPLACE
+  FUNCTION to_file(i_target_name VARCHAR2,
+                   i_param_table VARCHAR2,
+                   i_file_dir    VARCHAR2,
+                   i_file_name   VARCHAR2) RETURN VARCHAR2 IS
+    v_file_cur    Utl_File.file_type;
+    v_timestamp   NUMBER(20);
+    v_param_count NUMBER;
+    v_class_count NUMBER;
+    v_param_name  VARCHAR2(200);
+    v_value_char  VARCHAR2(100);
+    temp          NUMBER;
+  BEGIN
   
-    execute immediate 'select max(param_id) from ' || i_param_table ||
-                      ' where class_id = ' || v_temp1
-      into v_param_count;
-  
-    for v_temp2 in 1 .. v_param_count loop
+    --get timestamp
+    SELECT (sysdate - TO_DATE('19700101', 'yyyymmdd')) * 86400 - TO_NUMBER(SUBSTR(TZ_OFFSET(sessiontimezone), 1, 3)) * 3600
+    INTO v_timestamp
+    FROM dual;
     
-      execute immediate 'select param_name from ' || i_param_table ||
-                        ' where class_id = ' || v_temp1 ||
-                        ' and param_id = ' || v_temp2
-        into v_param_name;
-    
-      select count(*)
-        into temp
-        from calvalue_10min
-       where target_name = i_target_name
-         and param_name = v_param_name;
-      if temp = 0 then
-        v_value_char := '0';
-      else
-      
-        select case substr(to_char(param_value), 1, 1)
-                 when '.' then
-                  '0' || to_char(param_value)
-                 else
-                  to_char(param_value)
-               end
-          into v_value_char
-          from calvalue_10min
-         where target_name = i_target_name
-           and param_name = v_param_name;
-      
-        select case instr(v_value_char, '.')
-                 when 0 then
-                  v_value_char
-                 else
-                  substr(v_value_char, 0, instr(v_value_char, '.') + 2)
-               end
-          into v_value_char
-          from dual;
-      end if;
-    
-      if v_temp1 = 1 and v_temp2 = 1 then
-      
-        v_file_cur := utl_file.fopen(i_file_dir, i_file_name, 'w');
-      
-        Utl_file.putf(v_file_cur, v_timestamp || '  600  ' || '\n');
-      
-      end if;
-    
-      Utl_file.putf(v_file_cur, v_value_char || '  ');
-      if v_temp2 = v_param_count then
-        Utl_file.putf(v_file_cur, '\n');
-      end if;
-    
-    end loop;
-  end loop;
-  Utl_file.putf(v_file_cur, 'true' || '\n');
-  utl_file.fclose(v_file_cur);
 
-  return('true');
-end to_file;
-/
+    --get max class id,and loop for all classes.
+    --get max parameter id,and loop for all parameters.
+    EXECUTE immediate 'select max(class_id) from ' || i_param_table INTO v_class_count;
+    FOR v_temp1 IN 1 .. v_class_count LOOP
+      EXECUTE immediate 'select max(param_id) from ' || i_param_table || ' where class_id = ' || v_temp1 INTO v_param_count;
+      FOR v_temp2 IN 1 .. v_param_count LOOP
+      
+
+        --get the specified parameter
+        EXECUTE immediate 'select param_name from ' || i_param_table || ' where class_id = ' || v_temp1 || ' and param_id = ' || v_temp2 INTO v_param_name;
+        
+        --virify if the parameter have value
+        SELECT COUNT(*) INTO temp FROM calvalue_10min
+        WHERE target_name = i_target_name
+        AND param_name    = v_param_name;
+        
+        --if it don't have parameter value, then make it to '0'
+        IF temp = 0 THEN v_value_char   := '0';
+        ELSE
+        
+          --get parameter value,and format it if necessary.
+          SELECT CASE SUBSTR(TO_CHAR(param_value), 1, 1)
+                   WHEN '.' THEN '0' || TO_CHAR(param_value)
+                   ELSE TO_CHAR(param_value)
+                 END
+          INTO v_value_char
+          FROM calvalue_10min
+          WHERE target_name = i_target_name
+          AND param_name    = v_param_name;
+          
+          --Adjust the accuracy of the data
+          SELECT CASE instr(v_value_char, '.')
+                   WHEN 0 THEN v_value_char
+                   ELSE SUBSTR(v_value_char, 0, instr(v_value_char, '.') + 2)
+                 END
+          INTO v_value_char FROM dual;
+          
+        END IF;
+        
+        --If it's the first loop,then open and write basic informations into the output file.
+        IF v_temp1 = 1 AND v_temp2 = 1 THEN
+          v_file_cur := utl_file.fopen(i_file_dir, i_file_name, 'w');
+          Utl_file.putf(v_file_cur, v_timestamp || '  600  ' || '\n');
+        END IF;
+        
+        --Write parameter value into the output file,If at the end of class,then ENTER.
+        Utl_file.putf(v_file_cur, v_value_char || '  ');
+        IF v_temp2 = v_param_count THEN
+          Utl_file.putf(v_file_cur, '\n');
+        END IF;
+        
+      END LOOP;
+    END LOOP;
+    
+    --end and close the file.
+    Utl_file.putf(v_file_cur, 'true' || '\n');
+    utl_file.fclose(v_file_cur);
+    RETURN('true');
+  END to_file;
