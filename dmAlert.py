a73b1559183b9c9d4299ecf0c9aeec55aacdba54
oraant <oraant777@gmail.com>
2015-06-09 15:15:06 +0800

    add sql statements to get last hours data fo test.

--- dmAlert.py
+++ dmAlert.py
@@ -55,8 +55,23 @@ def getDynamic(cursor):
 	         group by target_name, param_name)
 	'''
 
-	cursor.execute(sql_get_last_month)
-	cursor.execute(sql_get_last_week)
+	sql_get_last_hour2 = '''
+	update alert_10min
+	   set last_month =
+	       (select min(param_value)
+	          from calvalue_arch
+	         where update_time >=
+	               sysdate - to_dsinterval('PT60M')
+	           and update_time <= sysdate
+	           and alert_10min.target_name = calvalue_arch.target_name
+	           and alert_10min.param_name = calvalue_arch.param_name
+	         group by target_name, param_name)
+	'''
+
+	#cursor.execute(sql_get_last_month)
+	#cursor.execute(sql_get_last_week)
+	cursor.execute(sql_get_last_hour)
+	cursor.execute(sql_get_last_hour2)
 	cursor.execute('commit')
 
 
@@ -85,6 +100,7 @@ def getAlert(cursor):
 	            on a.target_name = c.target_name
 	           and a.param_name = c.param_name)
 	 where param_value > alert_value
+         order by target_name
 	'''
 	cursor.execute(sql_get_alert)
 	datas = cursor.fetchall()
