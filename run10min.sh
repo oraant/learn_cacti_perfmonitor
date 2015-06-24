ed4377db09be882b37fa1c41cd954bd25f4c5bb9
oraant <oraant777@gmail.com>
2015-06-25 04:25:35 +0800

    add run shell for crontab.

---
+++ run10min.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PATH=$PATH:$HOME/bin
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u01/app/oracle/product/11.2.0/db_1/lib/
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/db_1
+export ORACLE_UNQNAME=db11g
+export ORACLE_SID=db11g
+export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch:$PATH
+
+export PATH
+export LD_LIBRARY_PATH
+export NLS_LANG='SIMPLIFIED CHINESE_CHINA.AL32UTF8'
+
+
+sleep 20;/usr/bin/python /perfmonitor/dataManager.py >> /perfmonitor/run.log
+sleep 10;/usr/bin/python /perfmonitor/tbs_dg.py >> /perfmonitor/run.log
---
+++ run1min.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PATH=$PATH:$HOME/bin
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u01/app/oracle/product/11.2.0/db_1/lib/
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/db_1
+export ORACLE_UNQNAME=db11g
+export ORACLE_SID=db11g
+export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/OPatch:$PATH
+
+export PATH
+export LD_LIBRARY_PATH
+export NLS_LANG='SIMPLIFIED CHINESE_CHINA.AL32UTF8'
+
+
+/usr/bin/python /perfmonitor/threshold.py >> /perfmonitor/run.log
