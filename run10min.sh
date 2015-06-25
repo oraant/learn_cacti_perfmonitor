61f93408558bc9bccbd39dad2c549ca1cf3454af
oraant <oraant777@gmail.com>
2015-06-25 17:58:40 +0800

    output error logs in run1min.sh and run10min.sh.

--- run10min.sh
+++ run10min.sh
@@ -13,5 +13,5 @@ export LD_LIBRARY_PATH
 export NLS_LANG='SIMPLIFIED CHINESE_CHINA.AL32UTF8'
 
 
-sleep 20;/usr/bin/python /perfmonitor/dataManager.py >> /perfmonitor/run.log
-sleep 10;/usr/bin/python /perfmonitor/tbs_dg.py >> /perfmonitor/run.log
+sleep 20;/usr/bin/python /perfmonitor/dataManager.py 1>>/perfmonitor/run.log 2>>/perfmonitor/run.err
+sleep 10;/usr/bin/python /perfmonitor/tbs_dg.py 1>>/perfmonitor/run.log 2>>/perfmonitor/run.err
--- run1min.sh
+++ run1min.sh
@@ -13,4 +13,4 @@ export LD_LIBRARY_PATH
 export NLS_LANG='SIMPLIFIED CHINESE_CHINA.AL32UTF8'
 
 
-/usr/bin/python /perfmonitor/threshold.py >> /perfmonitor/run.log
+/usr/bin/python /perfmonitor/threshold.py 1>>/perfmonitor/run.log 2>>/perfmonitor/run.err
