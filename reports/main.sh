440a14b5e9f8675448bb1965633b7aff309edbbb
oraant <oraant777@gmail.com>
2015-06-26 01:07:46 +0800

    move report file to another folder.

--- reports/main.sh
+++ reports/main.sh
@@ -15,4 +15,7 @@ filename=`date +%Y%m%d_%H%M`
 /perfmonitor/reports/sta_make.sh $link $inst_num $filename >> /home/oracle/main.log
 
 #####发邮件
-python /perfmonitor/reports/sendmail.py -f zyrmail@perfm.mail -p zyrmail -s perfm.mail -t test@perfm.mail -m "数据库 $link 的AWR, AWR_DIFF, ASH, ADDM, STA 报告已生成，详情请见附件。" /home/oracle/awrrpt_$filename.txt /home/oracle/ashrpt_$filename.txt /home/oracle/awrdiff_$filename.txt /home/oracle/top_sql_elps_$filename.txt /home/oracle/sta_$filename.txt /home/oracle/addm_$filename.txt > /dev/null 2>>/home/oracle/main.log 
+python /perfmonitor/reports/sendmail.py -f zyrmail@perfm.mail -p zyrmail -s perfm.mail -t test@perfm.mail -m "数据库 $link 的AWR, AWR_DIFF, ASH, ADDM, STA 报告已生成，详情请见附件。" /home/oracle/awrrpt_$filename.txt /home/oracle/ashrpt_$filename.txt /home/oracle/awrdiff_$filename.txt /home/oracle/top_sql_elps_$filename.txt /home/oracle/sta_$filename.txt /home/oracle/addm_$filename.txt > /dev/null 2>>/home/oracle/main.log
+
+#####转移文件到指定目录
+mv /home/oracle/*$filename* /home/oracle/perf_reports >> /home/oracle/main.log 2>&1
