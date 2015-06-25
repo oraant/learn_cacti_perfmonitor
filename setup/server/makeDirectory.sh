ae25a9a57bf7e1d483cc69545d5520d3e77126e2
oraant <oraant777@gmail.com>
2015-06-25 22:21:13 +0800

    add snmp and log directory for server/makeDirectory.sh

--- setup/server/makeDirectory.sh
+++ setup/server/makeDirectory.sh
@@ -1,3 +1,6 @@
 mkdir -p /cismon/datas/oracle10g
-mkdir -p /cismon/datas/oracle11g
+mkdir /cismon/datas/oracle11g
+mkdir /cismon/log
+mkdir /cismon/snmp
 chown oracle:oinstall -R /cismon
+chmod 777 -R /cismon
