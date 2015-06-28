b09bf8008b8bee98ffd7712fa3ad232760fd8863
oraant <oraant777@gmail.com>
2015-06-28 15:47:22 +0800

    Repair bug in base_line.sql,make sure we can got the right snap_id.

--- setup/client/base_line.sql
+++ setup/client/base_line.sql
@@ -2,7 +2,7 @@
 
 select * from dba_hist_snapshot order by snap_id desc;
 
-select * from (select snap_id from dba_hist_snapshot  order by snap_id desc) where rownum < 10;
+select * from (select snap_id,dbid,instance_number,startup_time from dba_hist_snapshot  order by snap_id desc) where rownum < 10;
 
 begin
  DBMS_WORKLOAD_REPOSITORY.CREATE_BASELINE(start_snap_id => 1822,end_snap_id => 1831,baseline_name => 'perfbl');
